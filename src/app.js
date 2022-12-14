6734fc5a0899d4828687509765a98dc3aad6b7ca
Lilin <cthulaura@gmail.com>
2022-12-14 15:20:53 -0300

    Added swagger documentation route

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger/swagger_output.json');
 require('dotenv-safe').config();
 const express = require('express');
 const cors = require('cors');
@@ -5,11 +7,9 @@ const mongoose = require('./database/dbConnect');
 const usersRoutes = require('./routes/usersRoutes');
 const worksRoutes = require('./routes/worksRoutes');
 const index = require("./routes/index");
-const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('../swagger/swagger_output.json');
+
+
 const app = express();
-
-
 app.use(express.json());
 app.use(cors());
 mongoose.connect();
--- swagger.js
+++ swagger.js
@@ -2,3 +2,4 @@ const swaggerAutogen = require('swagger-autogen')();
 const outputFile = './swagger/swagger_output.json';
 const endpointsFiles = ['./src/app.js'];
 swaggerAutogen(outputFile, endpointsFiles);
+
